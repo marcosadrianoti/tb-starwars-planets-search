7186b1046940ee3e95c90e525d6e2637cf97c9b5
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-02-27 15:06:24 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <span>Hello, App!</span>
-  );
+  return <span>Hello, App!!!</span>;
 }
 
 export default App;
