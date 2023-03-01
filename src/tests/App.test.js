348d1a6a1a15299f798edde1859463f8f2deb289
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-03-01 20:23:08 -0300

    Requisite 08

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,7 +5,7 @@ import PlanetsProvider from '../contexts/PlanetsProvider';
 import userEvent from '@testing-library/user-event';
 import mockPlanets from './mockPlanets';
 
-describe('Testando App', () => {
+describe('Testando se os filtros são funcionam adequadamente no App', () => {
   beforeEach(async () => {
     global.fetch = jest.fn(() => Promise.resolve({
       json: () => Promise.resolve(mockPlanets),
@@ -19,7 +19,7 @@ describe('Testando App', () => {
     })
   })
 
-  test('menor que', async () => {
+  test('Filtro - menor que', async () => {
     const filterByNameInput = await screen.findByTestId('name-filter');
     const columnFilterSelect = await screen.findByTestId('column-filter');
     const comparisonFilterSelect = screen.getByTestId('comparison-filter');
@@ -38,36 +38,37 @@ describe('Testando App', () => {
     userEvent.selectOptions(comparisonFilterSelect, 'menor que');
     userEvent.type(valueFilterInput, '9000');
     userEvent.click(btnFilter);
-    const planetsTableThanLess = await screen.findAllByTestId('planet-name');
-    expect(planetsTableThanLess).toHaveLength(3);
+    const beforePlanetsTableThanLess = await screen.findAllByTestId('planet-name');
+    expect(beforePlanetsTableThanLess).toHaveLength(3);
+    const btnRemove = screen.getByRole('button', { name: /apagar/i });
+    userEvent.click(btnRemove);
+    const afterPlanetsTableThanLess = await screen.findAllByTestId('planet-name');
+    expect(afterPlanetsTableThanLess).toHaveLength(10);
 
     userEvent.type(filterByNameInput, 'oo');
     const planetsTableByName = await screen.findAllByTestId('planet-name');
     expect(planetsTableByName).toHaveLength(2);
   });
-  test('maior que', async () => {
+  test('Filtro - maior que', async () => {
     const filterByNameInput = await screen.findByTestId('name-filter');
     const columnFilterSelect = await screen.findByTestId('column-filter');
     const comparisonFilterSelect = screen.getByTestId('comparison-filter');
     const valueFilterInput = screen.getByTestId('value-filter');
     const btnFilter = screen.getByTestId('button-filter');
     const planetsTable = await screen.findAllByTestId('planet-name');
-
-    expect(filterByNameInput).toBeInTheDocument();
-    expect(columnFilterSelect).toBeInTheDocument();
-    expect(comparisonFilterSelect).toBeInTheDocument();
-    expect(valueFilterInput).toBeInTheDocument();
-    expect(btnFilter).toBeInTheDocument();
-    expect(planetsTable).toHaveLength(10);
 
     userEvent.selectOptions(columnFilterSelect, 'orbital_period');
     userEvent.selectOptions(comparisonFilterSelect, 'maior que');
     userEvent.type(valueFilterInput, '4000');
     userEvent.click(btnFilter);
-    const planetsTableBiggerThen = await screen.findAllByTestId('planet-name');
-    expect(planetsTableBiggerThen).toHaveLength(2);
+    const beforePlanetsTableBiggerThen = await screen.findAllByTestId('planet-name');
+    expect(beforePlanetsTableBiggerThen).toHaveLength(2);
+    const btnRemove = screen.getByRole('button', { name: /apagar/i });
+    userEvent.click(btnRemove);
+    const afterPlanetsTableBiggerThen = await screen.findAllByTestId('planet-name');
+    expect(afterPlanetsTableBiggerThen).toHaveLength(10);
   })
-  test('igual a', async () => {
+  test('Filtro - igual a', async () => {
     const filterByNameInput = await screen.findByTestId('name-filter');
     const columnFilterSelect = await screen.findByTestId('column-filter');
     const comparisonFilterSelect = screen.getByTestId('comparison-filter');
@@ -75,18 +76,34 @@ describe('Testando App', () => {
     const btnFilter = screen.getByTestId('button-filter');
     const planetsTable = await screen.findAllByTestId('planet-name');
 
-    expect(filterByNameInput).toBeInTheDocument();
-    expect(columnFilterSelect).toBeInTheDocument();
-    expect(comparisonFilterSelect).toBeInTheDocument();
-    expect(valueFilterInput).toBeInTheDocument();
-    expect(btnFilter).toBeInTheDocument();
-    expect(planetsTable).toHaveLength(10);
+    userEvent.selectOptions(columnFilterSelect, 'rotation_period');
+    userEvent.selectOptions(comparisonFilterSelect, 'igual a');
+    userEvent.type(valueFilterInput, '23');
+    userEvent.click(btnFilter);
+    const beforePlanetsTableEqual = await screen.findAllByTestId('planet-name');
+    expect(beforePlanetsTableEqual).toHaveLength(3);
+    const btnRemove = screen.getByRole('button', { name: /apagar/i });
+    userEvent.click(btnRemove);
+
+    const afterPlanetsTableEqual = await screen.findAllByTestId('planet-name');
+    expect(afterPlanetsTableEqual).toHaveLength(10);
+
+    // Testando se o botão de remover todos os filtros funciona.
 
     userEvent.selectOptions(columnFilterSelect, 'rotation_period');
     userEvent.selectOptions(comparisonFilterSelect, 'igual a');
     userEvent.type(valueFilterInput, '23');
     userEvent.click(btnFilter);
-    const planetsTableBiggerThen = await screen.findAllByTestId('planet-name');
-    expect(planetsTableBiggerThen).toHaveLength(3);
+
+    userEvent.selectOptions(columnFilterSelect, 'orbital_period');
+    userEvent.selectOptions(comparisonFilterSelect, 'maior que');
+    userEvent.type(valueFilterInput, '4000');
+    userEvent.click(btnFilter);
+
+    const removeAllFilters = screen.getByRole('button', { name: /remover filtros/i })
+    userEvent.click(removeAllFilters);
+    const allPlanetsTable = await screen.findAllByTestId('planet-name');
+    expect(allPlanetsTable).toHaveLength(10);
+
   })
 });
